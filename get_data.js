e4e8c2f0f2fc6eec0e25c8a49d77bab56e240682
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-11-13 21:56:16 +0700

    updated

---
+++ get_data.js
@@ -0,0 +1,6 @@
+import { get } from "https://jscroot.github.io/api/croot.js";
+import {setInner,addChild } from "https://jscroot.github.io/element/croot.js";
+import { URLGeoJson } from "./fuction";
+// import { responseData } from "../chap02/js/controller/controller";
+
+get(URLGeoJson,responseData);
